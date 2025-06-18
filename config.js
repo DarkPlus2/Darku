5c9ab29fb47198f19754bd31d112acee3075f319
DarkPlusX <158550285+DarkPlus2@users.noreply.github.com>
2025-06-18 19:57:59 +0600

    Update config.js

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ const config = {
   connections: [
     { name: "GitHub", icon: "fab fa-github", username: "@DarkPlus2" },
     { name: "Spotify", icon: "fab fa-spotify", username: "DarkPlusX" },
-    { name: "Crunchyroll", icon: "bxl bx-crunchyroll", username: "The54Dark" },
+    { name: "Crunchyroll", icon: "fan fa-heart", username: "The54Dark" },
   ],
 
   themes: [
